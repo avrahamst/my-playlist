6f93ad1050f3d69c9a99049560dae59e084cde83
avraham schmidt <avibatst@gmail.com>
2022-06-09 13:06:37 +0300

    ia

--- src/componnt/Main.js
+++ src/componnt/Main.js
@@ -1,10 +1,31 @@
-import { React, useContext, useState } from "react";
+import { React, useContext, useEffect, useState } from "react";
 import { UserContext } from "../context/UserContext";
 
 export default function Main() {
   const { user } = useContext(UserContext);
-  const [pplay, usePplay] = useState();
+  const [pplay, setPplay] = useState();
 
+  // const options = {
+  //   method: "GET",
+  //   headers: {
+  //     "X-RapidAPI-Host": "simple-youtube-search.p.rapidapi.com",
+  //     "X-RapidAPI-Key": "c4cddc9027mshd1e64670419fcbap1cdd9bjsn46d7894034bc",
+  //   },
+  // };
+  // useEffect(() => {
+  //   fetch(
+  //     "https://simple-youtube-search.p.rapidapi.com/playlist?list=PLillGF-RfqbZ2ybcoD2OaabW2P7Ws8CWu",
+  //     options
+  //   )
+  //     .then((response) => response.json())
+  //     .then((response) => {
+  //       console.log(response);
+  //       setPplay(response);
+  //     })
+  //     .catch((err) => {
+  //       console.error(err);
+  //     });
+  // });
   return (
     <div>
       <div className="log">hello {user.name}</div>
@@ -12,7 +33,7 @@ export default function Main() {
       <div>
         <input placeholder="serch" />
       </div>
-
+      <img src={pplay.result.thumbnail} />
       <br />
       <div></div>
     </div>
